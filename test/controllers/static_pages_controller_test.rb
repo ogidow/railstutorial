3056c2c6fe51924d8d04aeaaa17aa3c952e5f09c
ogidow <syuta.ogido@gmail.com>
2016-06-17 14:39:04 +0900

    テスト書いた

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,25 +8,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", @base_title
+    assert_select full_title("Home")
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", full_title("Help")
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", full_title("About")
   end
 
   test "should get contacet" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", full_title("Contact")
   end
 
 end
